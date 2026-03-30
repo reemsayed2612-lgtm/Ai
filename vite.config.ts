fe5a05cabe47c25e9aeaecab0703d6e1279b40ea
reemsayed2612-lgtm <reemsayed2612@gmail.com>
2026-03-30 16:36:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    // هذا السطر يرفع حد التحذير لـ 1600kb ليختفي اللون البرتقالي
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        // هذا الجزء يقسم المكتبات الكبيرة لملفات أصغر لتحسين الأداء
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
+    // سنرفع الحد فقط ليختفي التحذير البرتقالي ولا يتعطل الموقع
+    chunkSizeWarningLimit: 2000,
   },
 });
