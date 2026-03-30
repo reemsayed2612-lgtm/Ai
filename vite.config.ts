e3c4768c8d2d1b635d34a252d49b7bc3ac7bce39
reemsayed2612-lgtm <reemsayed2612@gmail.com>
2026-03-30 16:39:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // سنرفع الحد فقط ليختفي التحذير البرتقالي ولا يتعطل الموقع
-    chunkSizeWarningLimit: 2000,
-  },
 });
